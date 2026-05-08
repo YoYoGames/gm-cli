defe45a7f96b5a55e8d27ee4d78e15fb444449d2
Eli Adelhult <eadelhult@opera.com>
2026-05-08 10:43:29 +0200

    Rename common-compile-setup

--- src/commands/package/impl.ts
+++ src/commands/package/impl.ts
@@ -16,17 +16,14 @@
 
 import type { Context } from "~/context";
 import type { ProjectPath } from "~/project";
-import {
-  commonCompileSetup,
-  type CommonCliBuildFlags,
-} from "~/common-compile-setup";
+import { runBuildPipeline, type CommonCliBuildFlags } from "~/build-pipeline";
 
 export default async function (
   this: Context,
   flags: CommonCliBuildFlags & { output?: string },
   project?: ProjectPath,
 ): Promise<void> {
-  await commonCompileSetup(this, flags, project, {
+  await runBuildPipeline(this, flags, project, {
     type: "package",
     outputPath: flags.output,
   });
--- src/commands/run/impl.ts
+++ src/commands/run/impl.ts
@@ -16,15 +16,12 @@
 
 import type { Context } from "~/context";
 import type { ProjectPath } from "~/project";
-import {
-  commonCompileSetup,
-  type CommonCliBuildFlags,
-} from "~/common-compile-setup";
+import { runBuildPipeline, type CommonCliBuildFlags } from "~/build-pipeline";
 
 export default async function (
   this: Context,
   flags: CommonCliBuildFlags,
   project?: ProjectPath,
 ): Promise<void> {
-  await commonCompileSetup(this, flags, project, { type: "run" });
+  await runBuildPipeline(this, flags, project, { type: "run" });
 }
