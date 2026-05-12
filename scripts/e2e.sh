16cd29ef762c93dc3b8dcb6b2f5b47e4210c031f
Eli Adelhult <eadelhult@opera.com>
2026-05-12 15:01:49 +0200

    Add e2e test for GMRT

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -7,29 +7,37 @@ GM_CACHE_DIR="$(pwd)/.gmcache"
 run_test() {
   local template="$1"
   local target="$2"
+  local toolchain="${3:-}"
 
-  echo "=== E2E: template='$template' target='$target' ==="
+  echo "=== E2E: template='$template' target='$target' toolchain='${toolchain:-default}' ==="
 
   rm -rf test-game
   node "$GM_COMMAND" init --template "$template" --name="test-game" --no-interactive --cache-dir "$GM_CACHE_DIR"
   cd test-game
 
+  local compile_args=(--target "$target" --cache-dir "$GM_CACHE_DIR")
+  if [ -n "$toolchain" ]; then
+    compile_args+=(--toolchain "$toolchain")
+  fi
+
   echo "--- compile (cold) ---"
-  node "$GM_COMMAND" compile --target "$target" --cache-dir "$GM_CACHE_DIR"
+  node "$GM_COMMAND" compile "${compile_args[@]}"
 
   echo "--- compile (warm) ---"
-  node "$GM_COMMAND" compile --target "$target" --cache-dir "$GM_CACHE_DIR"
+  node "$GM_COMMAND" compile "${compile_args[@]}"
 
   echo "--- resourcetool ---"
   node "$GM_COMMAND" resourcetool eval "resource list" --cache-dir "$GM_CACHE_DIR"
 
   cd ..
-  echo "=== PASSED: '$template' / $target ==="
+  echo "=== PASSED: '$template' / $target / toolchain='${toolchain:-default}' ==="
 }
 
 export NO_COLOR=1
 
 run_test "Blank Game" operagx
+
+#run_test "Blank Game" operagx gmrt
 
 # let's save some CI resources and only run multiple games on Windows!
 
