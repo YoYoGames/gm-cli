6e9c52e85a59c4bfa1a2bda02f455688ea8a1e3c
Eli Adelhult <eadelhult@opera.com>
2026-05-18 11:27:48 +0200

    Include package in E2E tests

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -6,16 +6,19 @@ GM_CACHE_DIR="$(pwd)/.gmcache"
 
 run_test() {
   local template="$1"
-  local target="$2"
+  local target="${2:-}"
   local toolchain="${3:-}"
 
-  echo "=== E2E: template='$template' target='$target' toolchain='${toolchain:-default}' ==="
+  echo "=== E2E: template='$template' target='${target:-default}' toolchain='${toolchain:-default}' ==="
 
   rm -rf test-game
   node "$GM_COMMAND" init --template "$template" --name="test-game" --no-interactive --cache-dir "$GM_CACHE_DIR"
   cd test-game
 
-  local compile_args=(--target "$target" --cache-dir "$GM_CACHE_DIR")
+  local compile_args=(--cache-dir "$GM_CACHE_DIR")
+  if [ -n "$target" ]; then
+    compile_args+=(--target "$target")
+  fi
   if [ -n "$toolchain" ]; then
     compile_args+=(--toolchain "$toolchain")
   fi
@@ -23,8 +26,15 @@ run_test() {
   echo "--- compile (cold) ---"
   node "$GM_COMMAND" compile "${compile_args[@]}"
 
-  echo "--- compile (warm) ---"
-  node "$GM_COMMAND" compile "${compile_args[@]}"
+  # Run again with a warm cache and the package command
+  # unless we are on a mac runner since it looks like  we need a signing key for the game
+  if [ "$(uname)" != "Darwin" ]; then
+    echo "--- package ---"
+    node "$GM_COMMAND" package "${compile_args[@]}"
+  else
+    echo "--- compile ---"
+    node "$GM_COMMAND" compile "${compile_args[@]}"
+  fi
 
   echo "--- resourcetool ---"
   node "$GM_COMMAND" resourcetool eval "resource list" --cache-dir "$GM_CACHE_DIR"
@@ -36,6 +46,7 @@ run_test() {
 export NO_COLOR=1
 
 run_test "Blank Game" operagx
+run_test "Blank Game"
 
 #run_test "Blank Game" operagx gmrt
 
